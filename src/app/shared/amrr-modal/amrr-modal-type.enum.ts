63cee81f4e91a1831e62461b61ff691283bd887d
AadhithyanBalaji <aadhithyanbalaji@gmai.com>
2023-02-24 20:32:48 +0530

    custom modal component

---
+++ src/app/shared/amrr-modal/amrr-modal-type.enum.ts
@@ -0,0 +1,4 @@
+export enum AmrrModalType{
+    Alert = 1,
+    Confirmation = 2
+}
--- src/app/shared/amrr-modal/amrr-modal.component.ts
+++ src/app/shared/amrr-modal/amrr-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, Input } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AmrrModalType } from './amrr-modal-type.enum';
 
 @Component({
   selector: 'app-amrr-modal',
@@ -11,16 +12,19 @@ export class AmrrModalComponent {
   body: string;
   actionText1: string;
   actionText2: string;
+  type: AmrrModalType;
+
+  AmrrModalType = AmrrModalType;
 
   constructor(
     public dialogRef: MatDialogRef<AmrrModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: AmrrModalComponent
   ) {
-    // Update view with given values
     this.title = data.title;
     this.body = data.body;
     this.actionText1 = data.actionText1 ?? 'Yes';
     this.actionText2 = data.actionText2 ?? 'No';
+    this.type = data.type ?? AmrrModalType.Confirmation;
   }
 
   onActionOne() {
