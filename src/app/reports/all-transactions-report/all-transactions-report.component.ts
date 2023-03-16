d51c6638f47178f70d53cef929c252ba2f4bfda2
AadhithyanBalaji <aadhithyanbalaji@gmai.com>
2023-03-16 14:17:26 +0530

    updated styling to reduce empty space at the top of the page

--- src/app/reports/all-transactions-report/all-transactions-report.component.ts
+++ src/app/reports/all-transactions-report/all-transactions-report.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { IAmmrGridColumn } from 'src/app/shared/ammr-grid/ammr-grid-column.interface';
 import { AmrrReportFilters } from 'src/app/shared/amrr-report-filters/amrr-report-filters.model';
-import { ApiBusinessService } from 'src/app/shared/api-business.service';
 import Helper from 'src/app/shared/helper';
 import { TransactionService } from 'src/app/shared/transaction.service';
 
@@ -16,17 +15,21 @@ export class AllTransactionsReportComponent {
   columns: IAmmrGridColumn[];
   loading = false;
 
-  constructor(private readonly transactionService: TransactionService) {
+  constructor(
+    private readonly transactionService: TransactionService
+  ) {
     this.transactionService.stockTransactions$.subscribe((data: any) => {
       this.dataSource = new MatTableDataSource(data.recordset);
-      const columnNames = Object.keys(this.dataSource.data[0]);
-      this.columns = [];
-      columnNames.forEach((col) => {
-        this.columns.push({
-          key: col,
-          name: col,
+      if (this.dataSource.data?.length > 0) {
+        const columnNames = Object.keys(this.dataSource.data[0]);
+        this.columns = [];
+        columnNames.forEach((col) => {
+          this.columns.push({
+            key: col,
+            name: col,
+          });
         });
-      });
+      }
       this.loading = false;
     });
   }
